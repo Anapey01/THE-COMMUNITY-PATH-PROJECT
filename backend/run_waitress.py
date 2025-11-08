8cbeb3d0d43167c9bfd0578d7631cc4dc6a42e7e
Anapey01 <gabriel.k.anapey@gmail.com>
2025-11-08 12:10:10 +0000

    Log starting port for Waitress server
    Added a print statement to indicate the starting port.

--- backend/run_waitress.py
+++ backend/run_waitress.py
@@ -1,13 +1,8 @@
-# run_waitress.py
 import os
 from waitress import serve
 from app import create_app
 
-# Create your Flask app
 app = create_app()
-
-# Get port from environment variable (Render sets this automatically)
 port = int(os.environ.get("PORT", 5000))
-
-# Serve with Waitress
+print(f"Starting app on port {port}...")
 serve(app, host="0.0.0.0", port=port)
